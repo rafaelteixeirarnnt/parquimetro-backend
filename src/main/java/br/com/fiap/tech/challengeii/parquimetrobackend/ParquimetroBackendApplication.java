3f6350339e7d37587eeb343804b669bd822938e9
Rafael Teixeira <rafael.teixeira@libertyti.com.br>
2024-01-13 09:34:48 -0300

    :tada: Iniciando o projeto

---
+++ src/main/java/br/com/fiap/tech/challengeii/parquimetrobackend/ParquimetroBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.challengeii.parquimetrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParquimetroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParquimetroBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/tech/challengeii/parquimetrobackend/ParquimetroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tech.challengeii.parquimetrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParquimetroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
